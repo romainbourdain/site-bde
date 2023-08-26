9de66f7ce916dd4aedf7c9c1cd26512f2778f985
Romain <romain.bourdain93@gmail.com>
2023-08-26 20:53:28 +0200

    responsive partenaires

--- src/pages/Partenaires/index.jsx
+++ src/pages/Partenaires/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import styled from "styled-components";
 import partenairesText from "../../assets/text/partenaires";
 import Navbar from "../../components/Navbar";
 import { Container } from "../../components/Section";
 import Title1 from "../../components/Titles/Title1";
 import Title2 from "../../components/Titles/Title2";
-import styled from "styled-components";
 
 const Partenaires = () => {
   return (
@@ -14,7 +14,41 @@ const Partenaires = () => {
         <Title1>{partenairesText.title}</Title1>
         {partenairesText.partenaires.map((partenaire, index) => (
           <div key={index}>
-            {index % 2 === 0 ? (
+            {window.innerWidth < 768 ? (
+              <LineContainer>
+                <Paragraph style={{ alignItems: "center" }}>
+                  <Title2
+                    style={{ textAlign: "center", marginBottom: "1.2rem" }}
+                  >
+                    {partenaire.name}
+                  </Title2>
+                  <Image
+                    src={partenaire.logo}
+                    alt={`${partenaire.name}-logo`}
+                  />
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      gap: "1.2rem",
+                    }}
+                  >
+                    {partenaire.offres && (
+                      <div>
+                        <h3>Offres</h3>
+                        {partenaire.offres}
+                      </div>
+                    )}
+                    {partenaire.conditions && (
+                      <div>
+                        <h3>Conditions</h3>
+                        {partenaire.conditions}
+                      </div>
+                    )}
+                  </div>
+                </Paragraph>
+              </LineContainer>
+            ) : index % 2 === 0 ? (
               <LineContainer>
                 <Paragraph>
                   <Title2
@@ -22,14 +56,18 @@ const Partenaires = () => {
                   >
                     {partenaire.name}
                   </Title2>
-                  <div>
-                    <h3>Offres</h3>
-                    {partenaire.offres}
-                  </div>
-                  <div>
-                    <h3>Conditions</h3>
-                    {partenaire.conditions}
-                  </div>
+                  {partenaire.offres && (
+                    <div>
+                      <h3>Offres</h3>
+                      {partenaire.offres}
+                    </div>
+                  )}
+                  {partenaire.conditions && (
+                    <div>
+                      <h3>Conditions</h3>
+                      {partenaire.conditions}
+                    </div>
+                  )}
                 </Paragraph>
                 <Image src={partenaire.logo} alt={`${partenaire.name}-logo`} />
               </LineContainer>
@@ -42,14 +80,18 @@ const Partenaires = () => {
                   >
                     {partenaire.name}
                   </Title2>
-                  <div>
-                    <h3>Offres</h3>
-                    {partenaire.offres}
-                  </div>
-                  <div>
-                    <h3>Conditions</h3>
-                    {partenaire.conditions}
-                  </div>
+                  {partenaire.offres && (
+                    <div>
+                      <h3>Offres</h3>
+                      {partenaire.offres}
+                    </div>
+                  )}
+                  {partenaire.conditions && (
+                    <div>
+                      <h3>Conditions</h3>
+                      {partenaire.conditions}
+                    </div>
+                  )}
                 </Paragraph>
               </LineContainer>
             )}
@@ -65,11 +107,16 @@ const LineContainer = styled.div`
   flex-direction: row;
   align-items: center;
   gap: 100px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 const Paragraph = styled.div`
   display: flex;
   flex-direction: column;
+
   gap: 1.2rem;
   flex: 1;
   margin-block: 2rem;
@@ -84,8 +131,9 @@ const Paragraph = styled.div`
 `;
 
 const Image = styled.img`
-  width: 300;
+  max-width: 300;
   height: 300;
+  margin-block: 2rem;
 `;
 
 export default Partenaires;
